d849cb4159b4175a6fc279ba9896a4def914f9b3
Dombis <dombis.gg@gmail.com>
2023-10-01 12:08:44 +0300

    Base command template was added

--- WSMS/ViewModels/Base/ViewModel.cs
+++ WSMS/ViewModels/Base/ViewModel.cs
@@ -6,7 +6,6 @@ namespace WSMS.ViewModels.Base
     internal abstract class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
---
+++ WSMS/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSMS.ViewModels.Base;
+
+namespace WSMS.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Title
+        private string _Title = "WSMS";
+        /// <summary Header MainWindow </summary>
+		public string Title
+		{
+			get => _Title;
+			set => Set(ref _Title, value);
+		}
+        #endregion
+    }
+}
