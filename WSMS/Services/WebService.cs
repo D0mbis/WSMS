bac6834efe5d52414d6a64442b5c231007c48cf8
Dombis <dombis.gg@gmail.com>
2023-10-02 16:25:44 +0300

    Update chromedriver was added

---
+++ WSMS/Services/WebService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
+using System.Windows;
+
+namespace WSMS.Services
+{
+    internal class WebService : IDisposable
+    {
+        private DriverManager? manager = null;
+        public void UpdateChromeDriver()
+        {
+            try
+            {
+                manager = new DriverManager();
+                manager.SetUpDriver(new ChromeConfig());
+                MessageBox.Show("Chromedriver was updated to {} version.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Chromedriver was not updated./n Error: {ex.Message}");
+            }
+        }
+        public void Dispose()
+        {
+            if (manager != null) { manager = default; }
+        }
+    }
+}
--- WSMS/ViewModels/MainWindowViewModel.cs
+++ WSMS/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WSMS.Infrastructure.Commands.Base;
+using WSMS.Services;
 using WSMS.ViewModels.Base;
 
 namespace WSMS.ViewModels
@@ -13,10 +17,41 @@ namespace WSMS.ViewModels
         private string _Title = "WSMS";
         /// <summary Header MainWindow </summary>
 		public string Title
-		{
-			get => _Title;
-			set => Set(ref _Title, value);
-		}
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
         #endregion
+
+        #region Comands
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object parameter) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+        #region UpdateChromeDriverComand
+        public ICommand UpdateChromeDriverComand { get; }
+
+        private bool CanUpdateChromeDriverComandExecute(object parameter) => true;
+
+        private void OnUpdateChromeDriverComandExecuted(object p)
+        {
+            using (WebService driver = new()) 
+            { 
+                driver.UpdateChromeDriver();
+            };
+        }
+        #endregion
+        #endregion
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            UpdateChromeDriverComand = new ActionCommand(OnUpdateChromeDriverComandExecuted, CanUpdateChromeDriverComandExecute);
+        }
     }
 }
